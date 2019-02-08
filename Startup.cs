4939e0827e173cfdb3f34171d0aac7f526dc2f82
Jeremy Caney <Jeremy.Caney@Ignia.com>
2019-02-08 01:11:13 -0800

    Apply the Ignia template to `Startup.cs`
    This includes commenting standards and basic documentation.

--- Startup.cs
+++ Startup.cs
@@ -1,4 +1,9 @@
-﻿using System;
+﻿/*==============================================================================================================================
+| Author        Ignia, LLC
+| Client        GoldSim
+| Project       Website
+\=============================================================================================================================*/
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -11,27 +16,72 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace OnTopicTest {
+
+  /*============================================================================================================================
+  | CLASS: STARTUP
+  \---------------------------------------------------------------------------------------------------------------------------*/
+  /// <summary>
+  ///   Configures the application and sets up dependencies.
+  /// </summary>
   public class Startup {
+
+    /*==========================================================================================================================
+    | CONSTRUCTOR
+    \-------------------------------------------------------------------------------------------------------------------------*/
+    /// <summary>
+    ///   Constructs a new instances of the <see cref="Startup"/> class. Accepts an <see cref="IConfiguration"/>.
+    /// </summary>
+    /// <param name="configuration">
+    ///   The shared <see cref="IConfiguration"/> dependency.
+    /// </param>
     public Startup(IConfiguration configuration) {
       Configuration = configuration;
     }
 
+    /*==========================================================================================================================
+    | PROPERTY: CONFIGURATION
+    \-------------------------------------------------------------------------------------------------------------------------*/
+    /// <summary>
+    ///   Provides a (public) reference to the application's <see cref="IConfiguration"/> service.
+    /// </summary>
     public IConfiguration Configuration { get; }
 
-    // This method gets called by the runtime. Use this method to add services to the container.
+    /*==========================================================================================================================
+    | METHOD: CONFIGURE SERVICES
+    \-------------------------------------------------------------------------------------------------------------------------*/
+    /// <summary>
+    ///   Provides configuration of services. This method is called by the runtime to bootstrap the server configuration.
+    /// </summary>
     public void ConfigureServices(IServiceCollection services) {
+
+      /*------------------------------------------------------------------------------------------------------------------------
+      | Configure: Cookie Policy
+      \-----------------------------------------------------------------------------------------------------------------------*/
       services.Configure<CookiePolicyOptions>(options => {
         // This lambda determines whether user consent for non-essential cookies is needed for a given request.
         options.CheckConsentNeeded = context => true;
         options.MinimumSameSitePolicy = SameSiteMode.None;
       });
 
-
+      /*------------------------------------------------------------------------------------------------------------------------
+      | Configure: MVC
+      \-----------------------------------------------------------------------------------------------------------------------*/
       services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+
     }
 
-    // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+    /*==========================================================================================================================
+    | METHOD: CONFIGURE (APPLICATION)
+    \-------------------------------------------------------------------------------------------------------------------------*/
+    /// <summary>
+    ///   Provides configuration the application. This method is called by the runtime to bootstrap the application
+    ///   configuration, including the HTTP pipeline.
+    /// </summary>
     public void Configure(IApplicationBuilder app, IHostingEnvironment env) {
+
+      /*------------------------------------------------------------------------------------------------------------------------
+      | Configure: Error Pages
+      \-----------------------------------------------------------------------------------------------------------------------*/
       if (env.IsDevelopment()) {
         app.UseDeveloperExceptionPage();
       }
@@ -41,15 +91,24 @@ namespace OnTopicTest {
         app.UseHsts();
       }
 
+      /*------------------------------------------------------------------------------------------------------------------------
+     | Configure: Server defaults
+     \-----------------------------------------------------------------------------------------------------------------------*/
       app.UseHttpsRedirection();
       app.UseStaticFiles();
       app.UseCookiePolicy();
 
+      /*------------------------------------------------------------------------------------------------------------------------
+      | Configure: MVC
+      \-----------------------------------------------------------------------------------------------------------------------*/
       app.UseMvc(routes => {
+
+        //Default
         routes.MapRoute(
-            name: "default",
-            template: "{controller=Home}/{action=Index}/{id?}");
+          name: "default",
+          template: "{controller=Home}/{action=Index}/{id?}");
       });
+
     }
   }
 }
