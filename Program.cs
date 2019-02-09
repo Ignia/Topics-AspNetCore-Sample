b7d21d115a920734634d54d32fecd4aea5ac8d9b
Jeremy Caney <Jeremy.Caney@Ignia.com>
2019-02-08 21:39:00 -0800

    Commented `Program.cs`
    No functional differences were introduced. The file was simply updated using Ignia's commenting standards.

--- Program.cs
+++ Program.cs
@@ -1,4 +1,9 @@
-﻿using System;
+﻿/*==============================================================================================================================
+| Author        Ignia, LLC
+| Client        Ignia, LLC
+| Project       Sample OnTopic Site
+\=============================================================================================================================*/
+using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
@@ -9,13 +14,35 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
 
 namespace OnTopicTest {
+
+  /*============================================================================================================================
+  | CLASS: PROGRAM
+  \---------------------------------------------------------------------------------------------------------------------------*/
+  /// <summary>
+  ///   The <see cref="Program"/> class—and it's <see cref="Program.Main(string[])"/> method—represent the entry point into the
+  ///   ASP.NET Core web application.
+  /// </summary>
   public class Program {
+
+    /*==========================================================================================================================
+    | METHOD: MAIN
+    \-------------------------------------------------------------------------------------------------------------------------*/
+    /// <summary>
+    ///   Responsible for bootstrapping the web application.
+    /// </summary>
     public static void Main(string[] args) {
       CreateWebHostBuilder(args).Build().Run();
     }
 
+    /*==========================================================================================================================
+    | METHOD: CREATE WEB HOST BUILDER
+    \-------------------------------------------------------------------------------------------------------------------------*/
+    /// <summary>
+    ///   Configures a new <see cref="IWebHostBuilder"/> with the default options.
+    /// </summary>
     public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
-        WebHost.CreateDefaultBuilder(args)
-            .UseStartup<Startup>();
-  }
-}
+      WebHost.CreateDefaultBuilder(args)
+        .UseStartup<Startup>();
+
+  } //Class
+} //Namespace
