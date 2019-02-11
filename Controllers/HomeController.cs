9af36732d4aea083ed2c99f4f2d4d6020bc3f143
Jeremy Caney <Jeremy.Caney@Ignia.com>
2019-02-10 21:14:35 -0800

    Commented OOTB classes
    Added comments to the `ErrorViewModel` and `HomeController` classes for consistency with Ignia's standards, and better documentation.

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,4 +1,9 @@
-﻿using System;
+﻿/*==============================================================================================================================
+| Author        Ignia, LLC
+| Client        Ignia, LLC
+| Project       Sample OnTopic Site
+\=============================================================================================================================*/
+using System;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
@@ -7,18 +12,45 @@ using Microsoft.AspNetCore.Mvc;
 using OnTopicTest.Models;
 
 namespace OnTopicTest.Controllers {
+
+  /*============================================================================================================================
+  | CLASS: HOME CONTROLLER
+  \---------------------------------------------------------------------------------------------------------------------------*/
+  /// <summary>
+  ///   Represents the default <see cref="Controller"/> for the web application.
+  /// </summary>
   public class HomeController : Controller {
+
+    /*==========================================================================================================================
+    | ACTION: INDEX
+    \-------------------------------------------------------------------------------------------------------------------------*/
+    /// <summary>
+    ///   The default action for incoming requests. Produces the homepage of the site.
+    /// </summary>
     public IActionResult Index() {
       return View();
     }
 
+    /*==========================================================================================================================
+    | ACTION: PRIVACY
+    \-------------------------------------------------------------------------------------------------------------------------*/
+    /// <summary>
+    ///   Produces the privacy statement for the site.
+    /// </summary>
     public IActionResult Privacy() {
       return View();
     }
 
+    /*==========================================================================================================================
+    | ACTION: ERROR
+    \-------------------------------------------------------------------------------------------------------------------------*/
+    /// <summary>
+    ///   Produces the error page for the site using an <see cref="ErrorViewModel"/>.
+    /// </summary>
     [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
     public IActionResult Error() {
       return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
     }
-  }
-}
+
+  } //Class
+} //Namespace
--- Models/ErrorViewModel.cs
+++ Models/ErrorViewModel.cs
@@ -1,9 +1,37 @@
+/*==============================================================================================================================
+| Author        Ignia, LLC
+| Client        Ignia, LLC
+| Project       Sample OnTopic Site
+\=============================================================================================================================*/
 using System;
 
 namespace OnTopicTest.Models {
+
+  /*============================================================================================================================
+  | CLASS: ERROR VIEW MODEL
+  \---------------------------------------------------------------------------------------------------------------------------*/
+  /// <summary>
+  ///   Represents the view state for an error page. Associated with <c>Error.cshtml</c>.
+  /// </summary>
   public class ErrorViewModel {
+
+    /*==========================================================================================================================
+    | PROPERTY: REQUEST ID
+    \-------------------------------------------------------------------------------------------------------------------------*/
+    /// <summary>
+    ///   Represents the current <see cref="Activity"/> identifier or, if unavailable, the <see cref="HttpContext"/>'s
+    ///   <c>TraceIdentifier</c>.
+    /// </summary>
     public string RequestId { get; set; }
 
+    /*==========================================================================================================================
+    | PROPERTY: SHOW REQUEST ID
+    \-------------------------------------------------------------------------------------------------------------------------*/
+    /// <summary>
+    ///   Determines whether or not the <see cref="RequestId"/> should be displayed; defauls to false if the <see
+    ///   cref="RequestId"/> value is not set.
+    /// </summary>
     public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-  }
-}
+
+  } //Class
+} //Namespace
