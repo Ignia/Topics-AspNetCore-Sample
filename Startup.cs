1f34b0e3ae75b09e427c1e46709a498fadc10e57
Jeremy Caney <Jeremy.Caney@Ignia.com>
2019-09-28 11:07:10 -0700

    Update compatibility version
    *.NET Core 3* apps are expected to run in `Version_3_0` MVC compatibility mode.

--- Startup.cs
+++ Startup.cs
@@ -74,7 +74,7 @@ namespace OnTopicTest {
       services.AddMvc()
 
         //Set to use .NET Core 2.2
-        .SetCompatibilityVersion(CompatibilityVersion.Version_2_2)
+        .SetCompatibilityVersion(CompatibilityVersion.Version_3_0)
 
         //Add OnTopic support
         .AddTopicSupport();
